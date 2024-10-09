ec347e7ce5cced75b7d28a1baa510c81b7174f00
unknown <shiva.dhungana2003@gmail.com>
2024-10-09 08:00:30 -0500

    Add connection behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{net::{Ipv4Addr, TcpListener}, ops::Add};
+use std:: thread;
 
 const OL_PORT : &str = "6969";
 
@@ -6,14 +7,19 @@ fn main()
 {
     let addr : String = Ipv4Addr::LOCALHOST.to_string().add(":").add(OL_PORT);
     let listener : TcpListener = TcpListener::bind(addr).unwrap();
-    accept_clients(listener);   
+    accept_clients(listener);
 }
 
-async fn accept_clients(listener : TcpListener)
+fn accept_clients(listener : TcpListener)
 {
     match listener.accept() 
     {
-        Ok((_socket, addr)) => println!("new client: {addr:?}"),
-        Err(e) => println!("couldn't get client: {e:?}")
+        Ok((_socket, addr)) => thread::spawn(|| on_connected),
+        Err(e) => panic!("{}", e)
     };
+}
+
+fn on_connected()
+{
+    println!("Client connected!");
 }
