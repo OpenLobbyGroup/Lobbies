96fa087835fb99e2c14f02741bb9c7e2006335a5
unknown <shiva.dhungana2003@gmail.com>
2024-10-16 12:02:13 -0500

    Add: basic html serving

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-use std::{net::{Ipv4Addr, TcpListener}, ops::Add};
+use std::io::{Read, Write};
+use std::{fs, net::{SocketAddr, TcpListener, TcpStream}};
 use std:: thread;
-
-const OL_PORT : &str = "6969";
 
 fn main() 
 {
-    let addr : String = Ipv4Addr::LOCALHOST.to_string().add(":").add(OL_PORT);
+    let addr  = "0.0.0.0:6969";
     let listener : TcpListener = TcpListener::bind(addr).unwrap();
-    accept_clients(listener);
+    loop 
+    {
+        accept_clients(&listener);
+    }
 }
 
-fn accept_clients(listener : TcpListener)
+fn accept_clients(listener : &TcpListener)
 {
-    match listener.accept() 
+    match listener.accept()
     {
-        Ok((_socket, addr)) => thread::spawn(|| on_connected),
-        Err(e) => panic!("{}", e)
+        Ok((stream, addr)) => 
+        {
+            thread::spawn(move || on_connected(stream, addr));
+        },
+        Err(e) => println!("Couldn't accept a client due to error {}", e)
     };
 }
 
-fn on_connected()
+fn on_connected(mut stream : TcpStream, _addr : SocketAddr)
 {
-    println!("Client connected!");
+    let mut buf = [0; 1024];
+    stream.read(&mut buf).unwrap();
+    println!("Received html:\n{:}", String::from_utf8_lossy(&buf));
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("Pages/page.html").unwrap();
+    let length = contents.len();
+    
+    let response =
+    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    
+    let count = stream.write(response.as_bytes()).unwrap();
+    println!("Sent back html in {count} bytes");
+    stream.shutdown(std::net::Shutdown::Both).unwrap();
 }
